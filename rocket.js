36a1367c0e02066a1a08a6211cfc4926cb6478cc
lea-stellini <lea.stellini@gmail.com>
2022-09-28 16:44:56 +0200

    added rocket state 3 => rocket go up

--- rocket.js
+++ rocket.js
@@ -8,14 +8,22 @@ const changeState = (state) => {
     document.getElementById('countdown').innerHTML = countdownNumber;
 
     //countdown
-    if(state == 2){
+    if(state === 2){
         timer = setInterval( () => {
             countdownNumber = countdownNumber-1;
             document.getElementById('countdown').innerHTML = countdownNumber;
 
             if(countdownNumber <= 0){
-                changeState(1);
+                changeState(3);
             };
         }, 500);
-    }
+    } else if (state === 3){
+        let success = setTimeout( () => {
+            let randomNumber = Math.round(Math.random()*10);
+
+            console.log('randomNumber:', randomNumber);
+        }, 2000);
+            
+       
+    };
 }
